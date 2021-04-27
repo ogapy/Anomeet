59968e0d5f6a5a22035fdd313ee68b4df427ae5d
ogapy <kyon6690@gmail.com>
2021-04-27 15:51:50 +0900

    [Fix] position javascript files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,7 @@ import "@fortawesome/fontawesome-free/js/all";
 import "../stylesheets/application";
 
 require("@rails/ujs").start();
-require("turbolinks").start();
+// require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("jquery");
-
-require("../script/script.js");
-require("../script/name_modal.js");
